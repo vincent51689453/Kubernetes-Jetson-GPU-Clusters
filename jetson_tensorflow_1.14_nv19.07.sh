5504c590ccc439c14ed45d1c0c8dfcde31e13de3
vincent51689453 <vincent51689453@gmail.com>
2020-05-04 10:52:29 +0800

    developing

---
+++ jetson_tensorflow_1.14_nv19.07.sh
@@ -0,0 +1,6 @@
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
+sudo apt-get install python3-pip
+sudo pip3 install -U pip testresources setuptools
+sudo pip3 install -U numpy==1.16.1 future==0.17.1 mock==3.0.5 h5py==2.9.0 keras_preprocessing==1.0.5 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
+sudo pip3 install --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu==1.14.0+nv19.7
