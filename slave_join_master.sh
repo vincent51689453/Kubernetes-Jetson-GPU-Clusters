691d94bb03aade978d914f5f9b274fa6ef530ae7
vincent51689453 <vincent51689453@gmail.com>
2020-05-05 13:58:15 +0800

    update token

--- slave_join_master.sh
+++ slave_join_master.sh
@@ -1,2 +1,2 @@
-sudo kubeadm join 192.168.0.10:6443 --token mzb7vf.00apte0yi1tha736 \
-    --discovery-token-ca-cert-hash sha256:1104f50809b76601cb4f702f04913c3f9758bc69f2222c2a8d0569fd0417b66f
+sudo kubeadm join 192.168.0.10:6443 --token 8nes1c.w1ut0m90oh1e0lvo \
+    --discovery-token-ca-cert-hash sha256:01544161b9e7c64bd21f5d45f080baa7b3ab6b1b52a461f94bdb9458748d0c56
