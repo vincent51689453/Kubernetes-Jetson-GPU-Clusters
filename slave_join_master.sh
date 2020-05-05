5132056703ac90ca5685bac3ed6497222b18f7da
vincent51689453 <vincent51689453@gmail.com>
2020-05-05 09:40:33 +0800

    add sudo authority for slave_join_master.sh

--- slave_join_master.sh
+++ slave_join_master.sh
@@ -1,2 +1,2 @@
-kubeadm join 192.168.0.10:6443 --token r2fo0q.ls1dqy9jx7t8ycmw \
+ sudo kubeadm join 192.168.0.10:6443 --token r2fo0q.ls1dqy9jx7t8ycmw \
     --discovery-token-ca-cert-hash sha256:88f24e70767dae6a0455ffc3245fcd5d9eb273b765b566f799c6669c50b285c1
